d589e3fcaebbf546b8e1902f41f7e183f85f9189
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-04-24 10:21:58 +0000

    automatic code generation

--- app/src/androidTest/java/com/sevban/tradejournal/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sevban/tradejournal/ExampleInstrumentedTest.kt
@@ -18,4 +18,5 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = Instrumentatio
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        
