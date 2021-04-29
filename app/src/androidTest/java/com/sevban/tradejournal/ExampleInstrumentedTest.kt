2269e30fae3d3d2e2037a9799257036a8eb4d96c
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-04-29 05:37:36 +0000

    miglioramento

--- app/src/androidTest/java/com/sevban/tradejournal/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/sevban/tradejournal/ExampleInstrumentedTest.kt
@@ -19,4 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.sevban.tradejournal", appC
+        assertEquals("com.sevban.tradejournal", appContext.packageName)
+    }
+}
