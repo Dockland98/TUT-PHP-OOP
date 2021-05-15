c82611f6b65734e508d99b56076f2ff373ab8ef4
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-05-15 14:33:55 +0000

    added newlines

--- app/src/main/java/com/sevban/tradejournal/TradeJournalApplication.kt
+++ app/src/main/java/com/sevban/tradejournal/TradeJournalApplication.kt
@@ -1,4 +1,7 @@
 package com.sevban.tradejournal
 
 import android.app.Application
-import dagger.hilt.android
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TradeJournalApplication:
