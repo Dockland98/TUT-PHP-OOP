3b6c21699767cb2e54703cef0a9ea8d10a9a5259
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-05-16 17:56:37 +0000

    extra check

--- app/src/main/java/com/sevban/tradejournal/di/FirebaseModule.kt
+++ app/src/main/java/com/sevban/tradejournal/di/FirebaseModule.kt
@@ -3,4 +3,6 @@ package com.sevban.tradejournal.di
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
-import dagger.Pro
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.comp
