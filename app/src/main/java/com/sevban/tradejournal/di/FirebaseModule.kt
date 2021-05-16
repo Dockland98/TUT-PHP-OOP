91a12ae3e6844ea5cbafd427e79f6c4ac70ac9cb
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-05-16 16:51:51 +0000

    Updated the ansible feedstock.

--- app/src/main/java/com/sevban/tradejournal/di/FirebaseModule.kt
+++ app/src/main/java/com/sevban/tradejournal/di/FirebaseModule.kt
@@ -1,4 +1,6 @@
 package com.sevban.tradejournal.di
 
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.Fireba
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Pro
