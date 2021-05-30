3e11b1589c2af718f41cd76511c14774be0777cf
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-05-30 10:09:21 +0000

    minor doc improvements

--- app/src/main/java/com/sevban/tradejournal/repo/PairDetailRepoInterface.kt
+++ app/src/main/java/com/sevban/tradejournal/repo/PairDetailRepoInterface.kt
@@ -2,4 +2,5 @@ package com.sevban.tradejournal.repo
 
 import android.content.Context
 import com.sevban.tradejournal.model.AnalyzeModel
-import com.s
+import com.sevban.tradejournal.util.Resource
+import kotlinx.coroutines.fl
