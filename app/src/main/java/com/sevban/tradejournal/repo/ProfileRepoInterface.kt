7cea328bcf19dc3a8e2aa48a4fc5d8b821532f1f
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-06-15 08:06:55 +0000

    Fix #98

--- app/src/main/java/com/sevban/tradejournal/repo/ProfileRepoInterface.kt
+++ app/src/main/java/com/sevban/tradejournal/repo/ProfileRepoInterface.kt
@@ -4,4 +4,6 @@ import androidx.compose.runtime.MutableState
 import com.sevban.tradejournal.model.AnalyzeModel
 import com.sevban.tradejournal.model.PairListModel
 import com.sevban.tradejournal.model.User
-import com.sevban.tradejournal.util.
+import com.sevban.tradejournal.util.Resource
+import kotlinx.coroutines.flow.Flow
+impo
