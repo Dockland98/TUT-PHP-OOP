5ffc8923311a06bd12928b99c78f22ba3ac2e6d1
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-09-11 11:49:07 +0000

    Small typos

--- app/src/main/java/com/sevban/tradejournal/ui/theme/Color.kt
+++ app/src/main/java/com/sevban/tradejournal/ui/theme/Color.kt
@@ -3,4 +3,6 @@ package com.sevban.tradejournal.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 =
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val
