224d775741d16585d751b7a9102882c29ae66be8
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-09-11 15:55:03 +0000

    Updated h_last_answers.md

--- app/src/main/java/com/sevban/tradejournal/ui/theme/Color.kt
+++ app/src/main/java/com/sevban/tradejournal/ui/theme/Color.kt
@@ -5,4 +5,7 @@ import androidx.compose.ui.graphics.Color
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
-val
+val Teal200 = Color(0xFF03DAC5)
+
+
+val Teal400 = Color(0xFF26a6
