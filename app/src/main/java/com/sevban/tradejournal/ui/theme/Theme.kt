5bddcb2caa80fb76595012b38f78eff1fad10019
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-09-22 13:59:36 +0000

    Tidy up formatting

--- app/src/main/java/com/sevban/tradejournal/ui/theme/Theme.kt
+++ app/src/main/java/com/sevban/tradejournal/ui/theme/Theme.kt
@@ -9,3 +9,39 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
+    background= Color(0XFF333333),
+    primary = Teal400Dark,
+    primaryVariant = Teal400,
+    secondary = Red700Dark
+)
+
+private val LightColorPalette = lightColors(
+    primary = Teal400Light,
+    primaryVariant = Teal400,
+    secondary = Red700Light
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun TradeJournalTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
