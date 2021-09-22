bd6c4d8f45627e17321873a699f655ae5d226f39
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-09-22 06:19:58 +0000

    more travis

--- app/src/main/java/com/sevban/tradejournal/ui/theme/Color.kt
+++ app/src/main/java/com/sevban/tradejournal/ui/theme/Color.kt
@@ -11,4 +11,6 @@ val Teal200 = Color(0xFF03DAC5)
 val Teal400 = Color(0xFF26a69a)
 val Teal400Text = Color(0xFF000000)
 val Teal400Light = Color(0xFF64d8cb)
-val Teal400Dark = Color(0xFF0076
+val Teal400Dark = Color(0xFF00766c)
+val Red700 = Color(0xFFd32f2f)
+val Red700Light = Col
