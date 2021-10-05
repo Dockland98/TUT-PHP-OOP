9813888bc3bf00842c867a65d09942a8fe42662a
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-10-05 19:14:05 +0000

    release 0.8.1

--- app/src/main/java/com/sevban/tradejournal/util/Resource.kt
+++ app/src/main/java/com/sevban/tradejournal/util/Resource.kt
@@ -1,4 +1,5 @@
 package com.sevban.tradejournal.util
 
 //Gelen verilerin sonuçlarını success, error ve loading olarak ele almaya yarayan sınıf.
-//Herhangi bir işlemin sonucunu bu üç durumda değerlend
+//Herhangi bir işlemin sonucunu bu üç durumda değerlendirmek için kullanılır.
+sealed class Resource <
