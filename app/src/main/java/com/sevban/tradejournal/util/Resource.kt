3b2df1b513fc4d40e81c001a26e8b3669169ab32
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-10-28 14:15:32 +0000

    Fix test config

--- app/src/main/java/com/sevban/tradejournal/util/Resource.kt
+++ app/src/main/java/com/sevban/tradejournal/util/Resource.kt
@@ -2,4 +2,6 @@ package com.sevban.tradejournal.util
 
 //Gelen verilerin sonuçlarını success, error ve loading olarak ele almaya yarayan sınıf.
 //Herhangi bir işlemin sonucunu bu üç durumda değerlendirmek için kullanılır.
-sealed class Resource <T> (val data: T?= null, val message: String?= nu
+sealed class Resource <T> (val data: T?= null, val message: String?= null){
+    class Success<T>(data: T?) : Resource<T> (data)
+    clas
