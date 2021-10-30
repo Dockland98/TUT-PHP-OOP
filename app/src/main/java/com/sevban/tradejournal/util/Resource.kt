58b0b9cb8e9e44036afe511a4fea297ce1641de7
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-10-30 09:58:40 +0000

    null upmerge

--- app/src/main/java/com/sevban/tradejournal/util/Resource.kt
+++ app/src/main/java/com/sevban/tradejournal/util/Resource.kt
@@ -5,4 +5,5 @@ package com.sevban.tradejournal.util
 sealed class Resource <T> (val data: T?= null, val message: String?= null){
     class Success<T>(data: T?) : Resource<T> (data)
     class Error <T> (message: String,data: T?=null): Resource<T>(data,message)
-    class Loading <T> : Resource<
+    class Loading <T> : Resource<T>()
+}
---
+++ app/src/main/java/com/sevban/tradejournal/util/SnapShotListenerWithCoroutines.kt
@@ -0,0 +1,3 @@
+package com.sevban.tradejournal.util
+
+imp
