052a5830b93a1f42bba8deb725fd31ab9687202f
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2021-11-07 11:52:25 +0000

    Propagated.

--- app/src/main/java/com/sevban/tradejournal/util/SnapShotListenerWithCoroutines.kt
+++ app/src/main/java/com/sevban/tradejournal/util/SnapShotListenerWithCoroutines.kt
@@ -3,4 +3,5 @@ package com.sevban.tradejournal.util
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
-impo
+import kotlinx.coroutines.channels.Channel
+import kotlin
