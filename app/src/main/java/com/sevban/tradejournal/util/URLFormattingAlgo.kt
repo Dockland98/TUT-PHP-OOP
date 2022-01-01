45258f519876ef0c85f889b84b3db50fefe58d52
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-01-01 15:10:26 +0000

    Translated by ohallot

--- app/src/main/java/com/sevban/tradejournal/util/URLFormattingAlgo.kt
+++ app/src/main/java/com/sevban/tradejournal/util/URLFormattingAlgo.kt
@@ -3,4 +3,4 @@ package com.sevban.tradejournal.util
 import java.util.regex.Pattern
 
 fun fetchIdOfImageFromURL(url: String?): String {
-   
+    if (url == null || !Pattern.matches("^https:\\/\\/www\\.
