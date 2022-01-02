3ee68bec9fa7c762475c67288ad0b3580e153fde
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-01-02 20:03:15 +0000

    Change author name

--- app/src/main/java/com/sevban/tradejournal/util/URLFormattingAlgo.kt
+++ app/src/main/java/com/sevban/tradejournal/util/URLFormattingAlgo.kt
@@ -5,4 +5,6 @@ import java.util.regex.Pattern
 fun fetchIdOfImageFromURL(url: String?): String {
     if (url == null || !Pattern.matches("^https:\\/\\/www\\.tradingview\\.com\\/x\\/[A-Za-z0-9]+$", url))
         throw java.lang.Exception("Entered URL does not match with a tradingview url chart link.")
-    val parts = 
+    val parts = url.split("/")
+    val id = parts[4]
+    val actualChart
