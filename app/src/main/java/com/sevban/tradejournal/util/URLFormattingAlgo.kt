bef3c3316a9a6653de1a064181c48e3072d1296a
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-01-02 07:15:50 +0000

    fixed #17

--- app/src/main/java/com/sevban/tradejournal/util/URLFormattingAlgo.kt
+++ app/src/main/java/com/sevban/tradejournal/util/URLFormattingAlgo.kt
@@ -4,4 +4,4 @@ import java.util.regex.Pattern
 
 fun fetchIdOfImageFromURL(url: String?): String {
     if (url == null || !Pattern.matches("^https:\\/\\/www\\.tradingview\\.com\\/x\\/[A-Za-z0-9]+$", url))
-     
+        throw java.lang.Exception("Entered URL does not ma
