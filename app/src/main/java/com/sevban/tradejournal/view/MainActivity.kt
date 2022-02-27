368cbccb6f2d12b938aee753ca299214a1474ff1
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-02-27 11:03:11 +0000

    ui update

--- app/src/main/java/com/sevban/tradejournal/view/MainActivity.kt
+++ app/src/main/java/com/sevban/tradejournal/view/MainActivity.kt
@@ -29,4 +29,5 @@ class MainActivity : ComponentActivity() {
             println("url is nullllllllllllll")
 
         setContent {
- 
+            TradeJournalTheme {
+                navController = r
