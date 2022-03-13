26ca5f4ab23c11df572cbefd60b69fd7ecc64050
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-03-13 08:19:26 +0000

    Minor mods.

--- app/src/main/java/com/sevban/tradejournal/view/NavGraph.kt
+++ app/src/main/java/com/sevban/tradejournal/view/NavGraph.kt
@@ -2,4 +2,5 @@ package com.sevban.tradejournal.view
 
 
 import android.content.Context
-import androidx.compose.runt
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
