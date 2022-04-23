a7d2b14e7dafcf5cbc998e2629775d3f614adef3
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-04-23 11:21:36 +0000

    update plug

--- app/src/main/java/com/sevban/tradejournal/view/components/AddPairBottomSheet.kt
+++ app/src/main/java/com/sevban/tradejournal/view/components/AddPairBottomSheet.kt
@@ -2,4 +2,6 @@ package com.sevban.tradejournal.view.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.r
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+impor
