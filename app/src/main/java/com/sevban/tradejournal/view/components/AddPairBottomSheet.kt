7640ed0b0a957887c6e59833fe9ad555667bd13b
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-04-23 19:57:02 +0000

    Edited readme.md

--- app/src/main/java/com/sevban/tradejournal/view/components/AddPairBottomSheet.kt
+++ app/src/main/java/com/sevban/tradejournal/view/components/AddPairBottomSheet.kt
@@ -4,4 +4,5 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-impor
+import androidx.compose.ui.Modifier
+import androidx.compose.
