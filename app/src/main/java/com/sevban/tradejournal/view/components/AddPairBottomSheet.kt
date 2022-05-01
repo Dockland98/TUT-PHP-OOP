e8ed4021385645c161843d29243767ec090c1dd6
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-05-01 09:55:38 +0000

    Added docs folder

--- app/src/main/java/com/sevban/tradejournal/view/components/AddPairBottomSheet.kt
+++ app/src/main/java/com/sevban/tradejournal/view/components/AddPairBottomSheet.kt
@@ -8,4 +8,4 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import andro
+import androidx.compose.foundation.shape.RoundedCornerShape 
