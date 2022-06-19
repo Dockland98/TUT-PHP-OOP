a7774dae988066cfbc31103e2d1b2eebabb05604
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-06-19 06:24:01 +0000

    fix fail

---
+++ app/src/main/java/com/sevban/tradejournal/view/components/Drawer/MenuItem.kt
@@ -0,0 +1,12 @@
+
+package com.sevban.tradejournal.view.components.Drawer
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MenuItem (
+
+    val id : String,
+    val title: String,
+    val contentDescription: String,
+    val icon: ImageVector
+        )
---
+++ app/src/main/java/com/sevban/tradejournal/view/components/Drawer/NavigationDrawer.kt
@@ -0,0 +1,51 @@
+
+package com.sevban.tradejournal.view
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sevban.tradejournal.view.components.Drawer.MenuItem
+
+
+@Composable
+fun DrawerHeader() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 64.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Trade Journal", fontSize = 40.sp)
+    }
+}
+
+@Composable
+fun DrawerBody(
+    items: List<MenuItem>,
+    modifier: Modifier = Modifier,
+    itemTextStyle: TextStyle = TextStyle(fontSize = 18.sp),
+    onItemClick: (MenuItem) -> Unit
+) {
+    LazyColumn(modifier) {
+        items(items) { item ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onItemClick(item) }
+                    .padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = item.icon,
+                    contentDescription = item.contentDescription
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+
+                Text(
