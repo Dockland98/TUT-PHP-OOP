fe75c1404be37e867235e849c053bd04405f6326
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-06-24 12:01:59 +0000

    Update DD.jl

--- app/src/main/java/com/sevban/tradejournal/view/components/Signing/SignTextAndButtons.kt
+++ app/src/main/java/com/sevban/tradejournal/view/components/Signing/SignTextAndButtons.kt
@@ -3,4 +3,5 @@ package com.sevban.tradejournal.view.components.Signing
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compos
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx
