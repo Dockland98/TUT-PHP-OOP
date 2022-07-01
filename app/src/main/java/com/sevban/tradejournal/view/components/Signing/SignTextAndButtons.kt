1f3141b0506be4cd04d04d8d563eb09d6f182933
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-07-01 16:14:43 +0000

    Release version 0.0.1

--- app/src/main/java/com/sevban/tradejournal/view/components/Signing/SignTextAndButtons.kt
+++ app/src/main/java/com/sevban/tradejournal/view/components/Signing/SignTextAndButtons.kt
@@ -10,4 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransform
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import android
