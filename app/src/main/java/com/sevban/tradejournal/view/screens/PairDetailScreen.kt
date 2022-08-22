342264223ab4c89aa540612c7db62a335d364f03
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-08-22 21:38:22 +0000

    update pywps

--- app/src/main/java/com/sevban/tradejournal/view/screens/PairDetailScreen.kt
+++ app/src/main/java/com/sevban/tradejournal/view/screens/PairDetailScreen.kt
@@ -1,2 +1,50 @@
 
 package com.sevban.tradejournal.view.screens
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
+import com.sevban.tradejournal.model.AnalyzeModel
+import com.sevban.tradejournal.util.Resource
+import com.sevban.tradejournal.util.fetchIdOfImageFromURL
+import com.sevban.tradejournal.view.components.AddPairBottomSheet
+import com.sevban.tradejournal.viewmodel.PairDetailViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun PairDetailScreen(
+    currencyId: String,
+    pairName: String,
+    navController: NavController,
+    urlFromIntent: String?,
+    viewModel: PairDetailViewModel = hiltViewModel()
+) {
+
+    val scope = rememberCoroutineScope()
