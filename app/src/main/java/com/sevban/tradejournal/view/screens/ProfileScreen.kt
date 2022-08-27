574bc8922a3971fd9f679884f08cc5f63740f1be
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-08-27 15:33:17 +0000

    Update upgrade.md

--- app/src/main/java/com/sevban/tradejournal/view/screens/ProfileScreen.kt
+++ app/src/main/java/com/sevban/tradejournal/view/screens/ProfileScreen.kt
@@ -32,3 +32,53 @@ import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.font.FontWeight.Companion.ExtraBold
+import androidx.compose.ui.text.font.FontWeight.Companion.Normal
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.sevban.tradejournal.R
+import com.sevban.tradejournal.model.AnalyzeModel
+import com.sevban.tradejournal.model.PairListModel
+import com.sevban.tradejournal.view.DrawerBody
+import com.sevban.tradejournal.view.DrawerHeader
+import com.sevban.tradejournal.view.components.Drawer.MenuItem
+import com.sevban.tradejournal.view.components.SearchBar
+import com.sevban.tradejournal.viewmodel.ProfileViewModel
+import kotlinx.coroutines.launch
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ProfileScreen(navController: NavController, viewModel: ProfileViewModel = hiltViewModel()) {
+
+    val traders = arrayListOf(
+        R.drawable.trader,
+        R.drawable.trader1,
+        R.drawable.trader2,
+        R.drawable.trader3,
+        R.drawable.trader4,
+        R.drawable.trader5,
+        R.drawable.trader6,
+        R.drawable.trader7,
+    )
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    val sheetState = rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+    val bottomScaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = sheetState)
+
+    viewModel.getUserModel()
+
+    val model by remember { viewModel.modelState }
+    var imageState by remember { mutableStateOf(R.drawable.trader) }
+    var openDialog by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = 1) {
+        viewModel.getPairList()
+    }
+    var pairList = viewModel.pairListResponse
+    val isLoading = viewModel.isLoading
