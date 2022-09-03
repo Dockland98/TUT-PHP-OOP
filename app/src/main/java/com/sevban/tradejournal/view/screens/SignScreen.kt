f3c530239669bcc1dc22beb724f723b095d6ebb2
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-09-03 17:05:52 +0000

    Save.

--- app/src/main/java/com/sevban/tradejournal/view/screens/SignScreen.kt
+++ app/src/main/java/com/sevban/tradejournal/view/screens/SignScreen.kt
@@ -9,3 +9,59 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.sevban.tradejournal.util.Resource
+import com.sevban.tradejournal.view.components.Signing.EmailText
+import com.sevban.tradejournal.view.components.Signing.GeneralButton
+import com.sevban.tradejournal.view.components.Signing.PasswordText
+import com.sevban.tradejournal.viewmodel.SignViewModel
+
+@Composable
+fun SignScreen(
+    viewModel: SignViewModel = hiltViewModel(),
+    navController: NavController,
+    context: Context
+) {
+    var emailText by remember { mutableStateOf("") }
+    var passwordText by remember { mutableStateOf("") }
+    val showProgressBar by remember { viewModel.isLoading }
+
+    LaunchedEffect(true) {
+
+        when (viewModel.currentUserCheck()) {
+            is Resource.Success -> {
+                navController.navigate(ScreenHolder.ProfileScreen.route) {
+                    popUpTo(ScreenHolder.SigningScreen.route) {
+                        inclusive = true
+                    }
+                }
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    if (showProgressBar) {
+        Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+    } else {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background,
+            contentColor = MaterialTheme.colors.onBackground
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 5.dp),
+
+                ) {
+
+                Column(
