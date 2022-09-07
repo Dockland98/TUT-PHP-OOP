6fb938cc0f1d2087dcf48e2c0f4510e5a751b07c
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-09-07 16:35:48 +0000

    relative urls

--- app/src/main/java/com/sevban/tradejournal/view/screens/SignUpScreen.kt
+++ app/src/main/java/com/sevban/tradejournal/view/screens/SignUpScreen.kt
@@ -3,4 +3,6 @@ package com.sevban.tradejournal.view.screens
 import android.content.Context
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx
