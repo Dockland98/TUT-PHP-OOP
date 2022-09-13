7f1a60c36649f6142511d9d178305825ebc403a8
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-09-13 13:03:35 +0000

    Autoscaler - scaling up --autoscaler

--- app/src/main/java/com/sevban/tradejournal/view/screens/SignUpScreen.kt
+++ app/src/main/java/com/sevban/tradejournal/view/screens/SignUpScreen.kt
@@ -9,4 +9,5 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com
+import com.sevban.tradejournal.model.User
+import com.se
