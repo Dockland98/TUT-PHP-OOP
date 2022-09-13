37fdd65d32279fbef77f599885fba7825abfdd6c
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-09-13 12:32:05 +0000

    update analytics

--- app/src/main/java/com/sevban/tradejournal/view/screens/SignUpScreen.kt
+++ app/src/main/java/com/sevban/tradejournal/view/screens/SignUpScreen.kt
@@ -6,4 +6,6 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+i
