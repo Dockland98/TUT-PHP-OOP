feaf25227bb828a89bf95041ee83077918dfcf61
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-10-29 12:55:21 +0000

    Auto-generated-cl: translation import

--- app/src/main/java/com/sevban/tradejournal/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/sevban/tradejournal/viewmodel/SignUpViewModel.kt
@@ -3,4 +3,5 @@ package com.sevban.tradejournal.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
-import com.google.firebase.
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore
