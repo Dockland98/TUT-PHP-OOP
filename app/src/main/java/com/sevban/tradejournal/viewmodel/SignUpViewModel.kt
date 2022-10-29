985cd8d56fdac9f369b62e52cb53b110ab2c973d
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-10-29 19:02:11 +0000

    tv

--- app/src/main/java/com/sevban/tradejournal/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/sevban/tradejournal/viewmodel/SignUpViewModel.kt
@@ -5,4 +5,5 @@ import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.sevban.tradejournal.m
+import com.sevban.tradejournal.model.User
+import com.sevban.tradejournal.repo.SignUpRepoInter
