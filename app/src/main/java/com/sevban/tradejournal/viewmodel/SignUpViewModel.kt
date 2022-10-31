10d251710d6ac7eb3eedb42717834b5d9cfb5962
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-10-31 08:54:34 +0000

    Update page.html.twig

--- app/src/main/java/com/sevban/tradejournal/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/sevban/tradejournal/viewmodel/SignUpViewModel.kt
@@ -11,3 +11,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
