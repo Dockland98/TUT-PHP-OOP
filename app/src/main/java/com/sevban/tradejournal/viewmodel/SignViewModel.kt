79c1f0325daeabe899d2fa07a916cd3de91db965
Dockland98 <112323972+Dockland98@users.noreply.github.com>
2022-11-08 13:30:44 +0000

    Fix review comments.

--- app/src/main/java/com/sevban/tradejournal/viewmodel/SignViewModel.kt
+++ app/src/main/java/com/sevban/tradejournal/viewmodel/SignViewModel.kt
@@ -3,4 +3,6 @@ package com.sevban.tradejournal.viewmodel
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecyc
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import and
